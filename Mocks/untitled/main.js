de3da09bd5bee686480a53e1e092bd4c40fb8d0e
Mic Ruopp <micruopp@gmail.com>
2021-11-20 10:57:33 -0600

    Initial commit.

---
+++ Mocks/untitled/main.js
@@ -0,0 +1,11 @@
+window.onload = main;
+
+document.addEventListener('DOMContentLoaded', fn1);
+
+function main() {
+  console.log('window loaded.');
+}
+
+function fn1() {
+  console.log('DOM loaded.');
+}
