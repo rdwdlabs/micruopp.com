76a75025f7dd945ae8153089acd09126bd46e02e
Mic Ruopp <micruopp@gmail.com>
2022-06-19 05:13:52 -0500

    Refactored script

--- main.js
+++ main.js
@@ -3,57 +3,50 @@
 // Copyright 2022. All rights reserved.
 
 (function() {
-  window.onload = beforeLaunch;
-  document.addEventListener('DOMContentLoaded', launchApp);
-
-  function beforeLaunch() {
-    console.log('window loaded.');
-    
-    // Set color scheme attributei
-    if (window.matchMedia && window.matchMedia('(prefers-color-scheme: light)').matches) {
-      document.documentElement.setAttribute('data-color-scheme', 'light');
-    } else {
-      document.documentElement.setAttribute('data-color-scheme', 'dark');
-    }
-  }
-
-  function launchApp() {
-    // What's the best adjective for this? link, bind, create
-    bindEventListeners();
-  }
-
-
-  function toggleDarkMode() {
-     setColorScheme(); 
-  }
-  
+  // Markup identifiers
   const SCHEME = 'data-color-scheme';
-  function __setColorScheme(el, scheme) {
-    let newScheme = (scheme === "dark") ? "light" : "dark";
-    el.setAttribute(SCHEME, newScheme);
-  }
-  
-  function setColorScheme() {
+
+  function toggleColorScheme() {
     const el = document.documentElement;
-    let colorScheme = el.getAttribute(SCHEME);
-  
-    __setColorScheme(el, colorScheme);
+    let currScheme = el.getAttribute(SCHEME);
+
+    // Detect OS default
+    let defaultScheme = 'dark';
+    if (window.matchMedia && window.matchMedia('(prefers-color-scheme: light)').matches) {
+      defaultScheme = 'light';
+    }
+    
+    let colorScheme = 'default';
+    switch (currScheme) {
+    case 'dark':
+      colorScheme = 'light';
+      break;
+    case 'light':
+      colorScheme = 'dark';
+      break;
+    default:
+      colorScheme = defaultScheme === 'dark' ? 'light' : 'dark';
+    }
+
+    el.setAttribute(SCHEME, colorScheme);
   }
   
   function handleKeyDown(e) {
-      switch (e.key) {
-      case 'd':
-        toggleDarkMode();
-        break;
-      default:
-      }
+    switch (e.key) {
+    case 'd':
+      toggleColorScheme();
+      break;
+    default:
+    }
+  }
+ 
+  function addEventListeners() {
+    window.addEventListener('keydown', handleKeyDown);
   }
   
-  function bindEventListeners() {
-    window.addEventListener('keydown', handleKeyDown);
-    
-    let schemeToggleButton = document.querySelector('.cst-button');
-    schemeToggleButton.addEventListener('click', toggleDarkMode);
-    // TODO: on window focus, CHECK dark mode and toggle if necessary.
+  function main() {
+    addEventListeners();
   }
+
+  document.addEventListener('DOMContentLoaded', main);
 })();
