65a4416ba93e572ffdd55d433660b7593d953236
Mic Ruopp <micruopp@gmail.com>
2024-06-03 01:24:48 -0500

    removed unused code

--- src/lib/keyboard.ts
+++ src/lib/keyboard.ts
@@ -11,26 +11,10 @@ interface KeyEvents {
 const keyFns: KeyEvents = {};
 
 function handleKeypress(e: any) {
-  // if keyFns[e.key]
-  //   do i preventdefault and stoppropagation?
-  //   or is that on the function to do?
-  //   keyFns[e.key]()
-  console.log("I'm handling the keypress! Woooo!");
-  const fn = keyFns[e.key];
-  if (fn) {
-    fn()
+  //   do i preventdefault and stoppropagation here?
+  if (e.key in keyFns) {
+    keyFns[e.key]();
   }
-  /*
-  switch (e.key) {
-    case 'd':
-    case 't':
-      e.preventDefault();
-      //toggleColorScheme();
-      e.stopPropagation();
-      break;
-    default:
-  }
-  */
 }
 
 export function setupKeyboardDelegate() {
@@ -42,10 +26,6 @@ export function removeKeyboardDelegate() {
 }
 
 export function registerKey(key: string, fn: () => void) {
-  console.log("registering key...")
-  console.log(key)
-  console.log(fn)
-  
   keyFns[key] = fn;
   // if !keyFns[key]
   //   keyFns[key] = fn
