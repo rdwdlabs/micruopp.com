fccc1b54ce854ff55086657557a7a270e53682ae
Mic Ruopp <micruopp@gmail.com>
2024-07-30 21:35:57 -0500

    removed copyright year

--- src/components/legal.tsx
+++ src/components/legal.tsx
@@ -2,7 +2,7 @@ export default function Legal() {
   return (
     <>
       {/*<p>Software may contain a LICENSE.</p>*/}
-      <p>Copyright &#169; 2024 Mic Ruopp. All rights reserved.</p>
+      <p>Copyright &#169; Mic Ruopp. All rights reserved.</p>
     </>
   );
 }
